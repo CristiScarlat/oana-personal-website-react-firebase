ef83712e186d5f4fdb6306dec8416083450c8783
CristiScarlat <78.cristi@gmail.com>
2021-02-17 21:47:24 +0200

    moved firebase variables to env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBq7EyP9V4TYFmrXcq_psEpiN_pIxyQp4",
-  authDomain: "cris-web-apps.firebaseapp.com",
-  projectId: "cris-web-apps",
-  storageBucket: "cris-web-apps.appspot.com",
-  messagingSenderId: "358125981958",
-  appId: "1:358125981958:web:1dd781a181ad9ab6e44e87",
-  measurementId: "G-2V42S5DF45"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
